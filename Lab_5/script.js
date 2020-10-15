3d286e17404fc30dbca8cff301fd264afb66e666
Olasunbo Salami <osalami@terpmail.umd.edu>
2020-10-15 19:18:55 -0400

    Update to #3

--- Lab_5/script.js
+++ Lab_5/script.js
@@ -1,4 +1,11 @@
+//** 2 created ordered list */
+let list = document.createElement('ol'); //created an ordered list
+
+//* 3 class for ordered list is countries*/
+list.className = "countries";
+
+
+
 //** 2 inject ordered list */
-let list = document.createElement('ol');
-let content = document.querySelector('#content');
-content.appendChild(list);
+let content = document.querySelector('#content'); //targeted the content class
+content.appendChild(list); //added div to this class
