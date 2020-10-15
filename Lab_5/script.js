30b49230877207b7d0c85906d78e758373b30ef4
Olasunbo Salami <osalami@terpmail.umd.edu>
2020-10-15 19:59:18 -0400

    Up to #4

--- Lab_5/script.js
+++ Lab_5/script.js
@@ -1,11 +1,24 @@
 //** 2 created ordered list */
-let list = document.createElement('ol'); //created an ordered list
+let list = document.createElement('ol');
 
 //* 3 class for ordered list is countries*/
 list.className = "countries";
 
+//** 4 target the button */
+const btn = document.querySelector('button')
+
+//** 5 function to select 25 random countries */
+function select25(){
+
+
+}
+
+//** 4 function runs on the click of button */
+btn.onclick = function(){
+    //7 use meh to add append list with random c
+}
 
 
 //** 2 inject ordered list */
-let content = document.querySelector('#content'); //targeted the content class
+let content = document.getElementsByClassName('content'); //targeted the content class
 content.appendChild(list); //added div to this class
