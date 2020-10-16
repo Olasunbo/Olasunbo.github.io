4c7116b7e5ba53c78f105c1dec8347a6cfeb6747
Olasunbo Salami <osalami@terpmail.umd.edu>
2020-10-15 22:31:55 -0400

    up to #6

--- Lab_5/script.js
+++ Lab_5/script.js
@@ -1,3 +1,28 @@
+/*//Call countries script
+let script = document.createElement('script'); 
+script.src = "countries.js" 
+const test = [
+    "Afghanistan"  ,
+      "Åland Islands",   "AX"  ,
+      "Albania",   "AL"  ,
+      "Algeria",   "DZ"  ,
+      "American Samoa",   "AS"  ,
+      "Andorra",   "AD"  ,
+      "Angola",   "AO"  ,
+      "Anguilla",   "AI"  ,
+      "Antarctica",   "AQ"  ,
+      "Antigua and Barbuda",   "AG"  ,
+      "Argentina",   "AR"  ,
+      "Armenia",   "AM"  ,
+      "Aruba",   "AW"  ,
+      "Australia",   "AU"  ,
+      "Austria",   "AT"  ,
+      "Azerbaijan",   "AZ"  ,
+      "Bahamas",   "BS"  ,
+      "Bahrain",   "BH"  ,
+]
+
+
 //** 2 created ordered list */
 let list = document.createElement('ol');
 
@@ -6,19 +31,28 @@ list.className = "countries";
 
 //** 4 target the button */
 const btn = document.querySelector('button')
+//document.querySelector('button').addEventListener("click",select25(countries))
 
 //** 5 function to select 25 random countries */
 function select25(array){
-    // Shuffle array
+    // 6 Shuffle array
     const shuffled = array.sort(() => 0.5 - Math.random());
     // Get sub-array of first n elements after shuffled
     let selected = shuffled.slice(0, 25);
-    return selected;
+    //might need to create an array not sure
+    return selected
 }
 
 //** 4 function runs on the click of button */
 btn.onclick = function(){
-    //7 use meh to add append list with random c
+    //run select25 function to get an array
+    let addme = select25(countries) 
+    // 7 add new list to old list
+    addme.forEach(function (nlist) {
+        let li = document.createElement('li');
+        li.textContent = nlist;
+        list.appendChild(li);
+    });
 }
 
 
