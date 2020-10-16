640a6b457adf13d5f9d8474cb3c06523af2105be
Olasunbo Salami <osalami@terpmail.umd.edu>
2020-10-15 23:57:11 -0400

    FINAL UPDATE

--- Lab_5/script.js
+++ Lab_5/script.js
@@ -6,9 +6,9 @@ list.className = "countries";
 
 //** 4 target the button */
 const btn = document.querySelector('button')
-//document.querySelector('button').addEventListener("click",select25(countries))
+//btn.addEventListener("click", select25()) Tried it
 
-//** 5 function to select 25 random countries */
+// 5 function to select 25 random countries 
 function select25(array){
     // 6 Shuffle array
     const shuffled = array.sort(() => 0.5 - Math.random());
@@ -22,7 +22,7 @@ function select25(array){
     return selected
 }
 
-//** 4 function runs on the click of button */
+//4 function runs on the click of button 
 btn.onclick = function(){
     //run select25 function to get an array
     let addme = select25(countries) 
@@ -35,6 +35,35 @@ btn.onclick = function(){
     });
 }
 
-//** 2 inject ordered list */
+// 2 inject ordered list 
 let content = document.getElementById('temp'); //targeted the content class
 content.appendChild(list)//added list to this class
+
+
+
+
+/* EXTRA CREDIT
+window.onload= function select25(){
+    // 6 Shuffle array
+    const shuffled = countries.sort(() => 0.5 - Math.random());
+    // 6 Get sub-array of first n elements after shuffled
+    let selected = shuffled.slice(0, 25);
+    // 10 get the rest of the list 
+    let x = shuffled.slice(25, shuffled.length - 1);
+    //10 Log of nonselected countries
+    console.log(x);
+    // 7 add new list to old list
+    selected.forEach(function (nlist) {
+        let li = document.createElement('li');
+        //8 & 9 normal and bold font
+        li.innerHTML = `${nlist.name}, <strong>${nlist.code}</strong>`;
+        list.appendChild(li);
+    });
+ //** 2 inject ordered list 
+let content = document.getElementById('temp'); //targeted the content class
+content.appendChild(list)//added list to this class   
+}; */
+
+
+
+
